7380452921234e561517ab4818c921e8a8ad38e2
Anemone <tttaina@mail.ru>
2019-12-13 01:18:33 +0300

    Scripted auto-commit on change (2019-12-13 01:18:33) by gitwatch.sh

--- src/main/java/AnonymizerApp.java
+++ src/main/java/AnonymizerApp.java
@@ -5,7 +5,10 @@ public class AnonymizerApp {
         //он указывает порт для каждого.
         if (args.length != 2) {
             System.out.println("Usage: AnonymizerApp <host> <post>");
+            System.exit(-1);
         }
+
+        
 
     }
 }
