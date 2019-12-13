60baba91284818350d8035439c7314f0d9f5d38a
Anemone <tttaina@mail.ru>
2019-12-13 03:57:46 +0300

    Scripted auto-commit on change (2019-12-13 03:57:46) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -1,10 +1,14 @@
 import akka.actor.AbstractActor;
 
+import java.util.List;
+
 public class StorageActor extends AbstractActor {
     //а. создаем актор хранилище конфигурации.
     //Он принимает две команды —
     //--список серверов (который отправит zookeeper watcher)
     //--запрос на получение случайного сервера
+    private List<String> storage;
+
     
     @Override
     public Receive createReceive() {
