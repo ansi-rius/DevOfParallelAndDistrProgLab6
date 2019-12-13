1e44b796e4fb2ef1dcc4dd871855d673dda4284c
Anemone <tttaina@mail.ru>
2019-12-13 04:00:39 +0300

    Scripted auto-commit on change (2019-12-13 04:00:39) by gitwatch.sh

--- src/main/java/StorageActor.java
+++ src/main/java/StorageActor.java
@@ -2,13 +2,21 @@ import akka.actor.AbstractActor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class StorageActor extends AbstractActor {
     //а. создаем актор хранилище конфигурации.
     //Он принимает две команды —
     //--список серверов (который отправит zookeeper watcher)
     //--запрос на получение случайного сервера
+
+    //-------------
+
+    //Разрабатываем akka http сервер который при получении запроса либо
+    //отправляет его на случайный сервер, уменьшая счетчик на 1 Либо
+    //осуществляет get для данного url и возвращает.
     private List<String> storage;
+    private Random randomServer;
 
     public StorageActor() {
         this.storage = new ArrayList<>();
