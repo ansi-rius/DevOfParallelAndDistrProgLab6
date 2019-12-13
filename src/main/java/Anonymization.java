0bdac446e44487a7d9cb8bfb8181f7815914b763
Anemone <tttaina@mail.ru>
2019-12-13 23:05:41 +0300

    Scripted auto-commit on change (2019-12-13 23:05:41) by gitwatch.sh

--- src/main/java/Anonymization.java
+++ src/main/java/Anonymization.java
@@ -72,9 +72,8 @@ public class Anonymization extends AllDirectives {
     private CompletionStage<HttpResponse> requestWithLowerCount(String url, int count, ActorSystem system) {
         return Patterns.ask(storage, new GetRandomServerMessage(), Duration.ofSeconds(3))
                 .thenApply(obj -> ((ReturnRandomServerMessage)obj).getServer())
-                .thenCompose(msg -> urlRequest(getUri(msg), system)
-                        .query()
-        ())
+                .thenCompose(msg -> 
+
     }
 
     public static Uri getUri(String adr) {
