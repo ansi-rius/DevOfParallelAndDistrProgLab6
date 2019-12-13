0ae089b4ecfdcf9b8c94a0fe69516c308f18ef05
Anemone <tttaina@mail.ru>
2019-12-13 19:21:57 +0300

    Scripted auto-commit on change (2019-12-13 19:21:57) by gitwatch.sh

--- src/main/java/Anonymization.java
+++ src/main/java/Anonymization.java
@@ -1,3 +1,16 @@
+import akka.actor.ActorRef;
+import org.apache.zookeeper.ZooKeeper;
+import org.asynchttpclient.AsyncHttpClient;
+
+
 public class Anonymization {
-    
+    private ActorRef storage;
+    private AsyncHttpClient http;
+    private ZooKeeper zoo;
+
+    public Anonymization(ActorRef storage, AsyncHttpClient http, ZooKeeper zoo) {
+        this.storage = storage;
+        this.http = http;
+        this.zoo = zoo;
+    }
 }
